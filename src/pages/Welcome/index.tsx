23bc63937b015104c1e7bc5244f88c529853e626
r0cc4t0 <guilherme.roccato@outlook.com>
2021-09-02 23:15:24 -0300

    Icons and Fonts

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { SafeAreaView, Text, Image } from 'react-native';
+import { SafeAreaView, View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import wateringImg from '../../assets/watering.png';
-import Button from '../../components/Button';
+import { Feather } from '@expo/vector-icons';
 
 export default function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie{'\n'}
-        suas plantas{'\n'}
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie{'\n'}
+          suas plantas de{'\n'}
+          forma fácil
+        </Text>
 
-      <Image style={styles.image} source={wateringImg} />
+        <Image style={styles.image} source={wateringImg} resizeMode="contain" />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.{'\n'}
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.{'\n'}
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
 
-      <Button title="Avançar" />
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,29 +1,49 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import colors from '../../../styles/colors';
+import fonts from '../../../styles/fonts';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 24
+    marginTop: 24,
+    fontFamily: fonts.heading,
+    lineHeight: 32
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 16,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
   },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 16,
+    height: 56,
+    width: 56
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white
+  }
 });
 
 export default styles;
